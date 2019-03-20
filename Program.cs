2ba10470555bab67fefaf57dca5a33951228b620
blakereeves19 <blake.reeves19@gmail.com>
2019-03-20 17:05:53 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Tom";
+            int characterAge;
+            characterAge = 35;
+
+            Console.WriteLine("There once was a man name " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
