c5905411878d354a2570aa5412930b8845b7ea2d
blakereeves19 <blake.reeves19@gmail.com>
2019-03-22 22:12:14 -0500

    practiced using comparisons in if statements in c#

--- Program.cs
+++ Program.cs
@@ -6,23 +6,29 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-
-            bool isMale = false;
-            bool isTall = true;
-
-            if (isMale && isTall)
-            {
-                Console.WriteLine("You are male or tall.");
-            } else if (isMale && !isTall) {
-                Console.WriteLine("You are male and not tall.");
-            } else if (!isMale && isTall) {
-                Console.WriteLine("You are not a male but are tall.");
-            } else
-            {
-                Console.WriteLine("You are not male and not tall.");
-            }
+            Console.WriteLine(GetMax(20, 100, 150));
 
             Console.ReadLine();
         }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int result;
+
+            if (num1 >= num2 && num1 >= num3)
+            {
+                result = num1;
+            } else if (num2 >= num1 && num2 >= num3)
+            {
+                result = num2;
+            }
+            else
+            {
+                result = num3;
+            }
+
+            return result;
+        }
+
     }
 }
